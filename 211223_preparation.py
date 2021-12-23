def020d1cd68bd4428588b29a1e9174e514d562f
sumin <tnals545@naver.com>
2021-12-23 21:31:45 +0900

    211223_자습1

---
+++ 211223_preparation.py
@@ -0,0 +1,21 @@
+# 001 print 기초
+# 화면에 Hello World 문자열을 출력하세요.
+print("Hello World") # 정답
+
+# 002 print 기초
+# 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
+print("Mary's consmetics") # 정답
+
+# 003 print 기초
+# 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)
+print('신씨가 소리질렀다. "도둑이야".') # 정답
+
+# 004 print 기초
+# 화면에 "C:\Windows"를 출력하세요.
+print("\"C:\Windows\"") # "" 이것도 출력되야 하는줄..   출력값 : "C:\Windows"
+# 해설 : print("C:\Windows")    출력값 : C:\Windows
+
+# 005 print 탭과 줄바꿈
+# 다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
+print("안녕하세요.\n만나서\t\t반갑습니다.") # \n은 줄바꿈, \t는 탭? 인거같다.
+# 해설 : \t는 탭을 의미하고 \n'은 줄바꿈을 의미합니다.
