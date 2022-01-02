fd269ca0d6ab683ba615a626031b26bd80799705
sumin <tnals545@naver.com>
2022-01-02 23:41:54 +0900

    220102 26~30

--- preview/211223_preparation.py
+++ preview/211223_preparation.py
@@ -190,3 +190,51 @@ print(phone_number.replace('-', ' ')) # 010 1111 2222
 # 해설
 # phone_number1 = phone_number.replace("-", " ")
 # print(phone_number1)
+
+# 026 문자열 다루기
+# 25번 문제의 전화번호를 아래와 같이 모두 붙여 출력하세요.
+phone_number = "010-1111-2222"
+print(phone_number.replace('-', '')) # 01011112222
+# 해설
+# phone_number = "010-1111-2222"
+# phone_number1 = phone_number.replace('-', '')
+# print(phone_number1)
+
+# 027 문자열 다루기
+# url 에 저장된 웹 페이지 주소에서 도메인을 출력하세요.
+url = "http://sharebook.kr"
+print(url.split('.')[1]) # kr
+# 해설
+# url = "http://sharebook.kr"
+# url_split = url.split('.')
+# print(url_split[-1])
+
+# 028 문자열은 immutable
+# 아래 코드의 실행 결과를 예상해보세요.
+lang = 'python'
+lang[0] = 'P'
+print(lang)
+# 내 예상 : Python
+# 해설
+# 문자열은 수정할 수 없습니다.
+# 실행 결과를 확인해보면 문자열이 할당(assignment) 메서드를 지원하지 않음을 알 수 있습니다.
+
+# 029 replace 메서드
+# 아래 문자열에서 소문자 'a'를 대문자 'A'로 변경하세요.
+string = 'abcdfe2a354a32a'
+string_A = string.replace('a', 'A')
+print(string_A) # Abcdfe2A354A32A
+# 해설
+# string = 'abcdfe2a354a32a'
+# string = string.replace('a', 'A')
+# print(string)
+
+# 030 replace 메서드
+# 아래 코드의 실행 결과를 예상해보세요.
+string = 'abcd'
+string.replace('b', 'B')
+print(string)
+# 내 예상 : 'abcd' >> 실행된 결과를 변수에 넣어두지 않았기 때문에 원본은 그대로다.
+# 해설
+# abcd가 그대로 출력됩니다. 왜냐하면 문자열은 변경할 수 없는 자료형이기 때문입니다.
+# replace 메서드를 사용하면 원본은 그대로 둔채로 변경된 새로운 문자열 객체를 리턴해줍니다.
