da3edde34266a3396b305c2c1098b5b41206ce21
sumin <tnals545@naver.com>
2022-01-24 18:02:31 +0900

    2022_01_24_training

--- training/20220119_Sorting.py
+++ training/20220119_Sorting.py
@@ -1,6 +1,8 @@
 ### 정렬(Sorting)
 # 데이터를 특정한 기준에 따라 순서대로 나열하는 것
 
+
+
 ## 선택 정렬
 # 처리되지 않은 데이터 중에서 가장 작은 데이터를 선택해 맨 앞에 있는 데이터와 바꾸는 것을 반복
 from cmath import pi
@@ -18,6 +20,8 @@ for i in range(len(array)):
 
 print(array)
 
+
+
 ## 삽입 정렬
 # 처리되지 않은 데이터를 하나씩 골라 적절한 위치에 삽입
 # 선택 정렬에 비해 구현 난이도가 높은 편이지만, 일반적으로 더 효율적으로 동작
@@ -32,6 +36,8 @@ for i in range(1, len(array)):
             break
 
 print(array)
+
+
 
 ## 퀵 정렬
 # 기준 데이터를 설정하고 그 기준보다 큰 데이터와 작은 데이터의 위치를 바꾸는 방법
@@ -81,3 +87,51 @@ def quick_sort(array):
     return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
 print(quick_sort(array))
+
+
+
+## 계수 정렬
+# 특정 조건이 부합할 때만 사용할 수 있는 '매우 빠르게 동작'하는 정렬 알고리즘
+# -> 데이터의 크기 범위가 제한되어 정수 형태로 표현할 수 있을 때 사용 가능
+# 데이터 갯수 N, 데이터(양수) 중 최댓값이 K 일때 수행 시간 O(N + K) 보장
+
+
+# 모든 원소의 값이 0보다 크거나 같다고 가정
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+# 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1 # 각 데이터에 해당하는 인덱의 값 증가
+
+for i in range(len(count)): # 리스트에 기록된 정렬 정보 확인
+    for j in range(count[i]):
+        print(i, end = ' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+
+
+# 계수 정렬의 시간 복잡도와 공간 복잡도는 모두 O(N + K)
+# 계수 정렬은 때에 따라서 심각한 비효율성을 초래
+# ex) 데이터가 0과 999,999로 단 2개만 존재하는 경우
+# 계수 정렬은 동일한 값을 가지는 데이터가 여러 개 등장할 때 효과적으로 사용 가능
+# ex) 성적의 경우 100점을 맞은 학생이 여러 명일 수 있기 때문에 계수 정렬 효과적
+
+
+
+# 두 배열의 원소 교체 : 답안 예시
+n, k = map(int, input().split()) # N과 K를 입력받기
+a = list(map(int, input().split())) # 배열 A의 모든 원소를 입력 받기
+b = list(map(int, input().split())) # 배열 B의 모든 원소를 입력 받기
+
+a.sort() # 배열 A는 오름차순 정렬 수행
+b.sort(reverse = True) # 배열 B는 내림차순 정렬 수행
+
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+    # A의 원소가 B의 원소보다 작을 경우
+    if a[i] < b[i]:
+        # 두 원소 교체
+        a[i], b[i] = b[i], a[i]
+    else: # A의 원소가 B의 원소보다 크거나 같을 때, 반복문을 탈출
+        break
+
+print(sum(a)) # 배열 A의 모든 원소의 합을 출력
