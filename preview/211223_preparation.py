7482fd8d679f1f9030de93289732a1fb0e7c24e7
sumin <tnals545@naver.com>
2022-04-20 23:35:09 +0900

    TIL_2022_04_20

--- preview/211223_preparation.py
+++ preview/211223_preparation.py
@@ -451,3 +451,54 @@ len(cook)
 nums = [1, 2, 3, 4, 5]
 
 np.mean(nums)
+
+# 61
+price = ['20180728', 100, 130, 140, 150, 160, 170]
+
+price[1:]
+
+# 62
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+nums[::2]
+
+# 63
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+nums[1::2]
+
+# 64
+nums = [1, 2, 3, 4, 5]
+
+nums[::-1]
+
+# 65
+interest = ['삼성전자', 'LG전자', 'Naver']
+
+print(interest[0] + ' ' + interest[2])
+
+# 66
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+
+" ".join(interest)
+
+# 67
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+
+"/".join(interest)
+
+# 68
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+
+print('\n'.join(interest))
+
+# 69
+string = "삼성전자/LG전자/Naver"
+
+string.split('/')
+
+# 70
+data = [2, 4, 3, 1, 5, 10, 9]
+
+data.sort()
+data
