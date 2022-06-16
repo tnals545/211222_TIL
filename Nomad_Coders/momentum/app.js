6a909757943c2667be97959a611c9bee52ae652e
sumin <tnals545@naver.com>
2022-06-16 16:27:59 +0900

    const and let

---
+++ Nomad_Coders/momentum/app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "sumin";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName)
+
+myName = "ChoiSuMin";
+
+console.log("your new name is " + myName)
