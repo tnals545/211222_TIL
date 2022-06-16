2777c633de3e56ce7c2672c6ce622e6ad0ea8168
sumin <tnals545@naver.com>
2022-06-16 17:05:19 +0900

    Booleans

--- Nomad_Coders/momentum/app.js
+++ Nomad_Coders/momentum/app.js
@@ -1,12 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "sumin";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName)
-
-myName = "ChoiSuMin";
-
-console.log("your new name is " + myName)
+const amIFat = null;
+let something;
+console.log(something, amIFat);
