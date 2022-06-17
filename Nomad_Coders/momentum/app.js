645b3be6b3a19d75bd01c6e68f61c837fdf35273
sumin <tnals545@naver.com>
2022-06-17 10:06:27 +0900

    Arrays

--- Nomad_Coders/momentum/app.js
+++ Nomad_Coders/momentum/app.js
@@ -1,3 +1,9 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
