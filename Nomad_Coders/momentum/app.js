1632978fae40d52ba845737609ef850781a24e7a
sumin <tnals545@naver.com>
2022-06-17 16:06:32 +0900

    Returns

--- Nomad_Coders/momentum/app.js
+++ Nomad_Coders/momentum/app.js
@@ -1,23 +1,23 @@
 const calculator = {
   add: function add(a, b) {
-    console.log(a + b);
+    return a + b;
   },
   min: function min(a, b) {
-    console.log(a - b);
+    return a - b;
   },
   div: function div(a, b) {
-    console.log(a / b);
+    return a / b;
   },
   mul: function mul(a, b) {
-    console.log(a * b);
+    return a * b;
   },
   pow: function pow(a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 
-calculator.add(1, 2);
-calculator.min(1, 2);
-calculator.div(1, 2);
-calculator.mul(1, 2);
-calculator.pow(1, 2);
+const addResult = calculator.add(2, 3);
+const minResult = calculator.min(addResult, 10);
+const mulResult = calculator.mul(10, minResult);
+const divResult = calculator.div(mulResult, addResult);
+const powResult = calculator.pow(divResult, minResult);
