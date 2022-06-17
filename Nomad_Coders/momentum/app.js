1e445a89537a529cfa055d6942aeeb8b04c28fb5
sumin <tnals545@naver.com>
2022-06-17 15:49:03 +0900

    Recap-My Calculator

--- Nomad_Coders/momentum/app.js
+++ Nomad_Coders/momentum/app.js
@@ -1,9 +1,23 @@
-const player = {
-    name:"sumin",
-    sayHello:function(otherPersonsName) {
-        console.log("hello " + otherPersonsName + " nice to meet you");
-    },
+const calculator = {
+  add: function add(a, b) {
+    console.log(a + b);
+  },
+  min: function min(a, b) {
+    console.log(a - b);
+  },
+  div: function div(a, b) {
+    console.log(a / b);
+  },
+  mul: function mul(a, b) {
+    console.log(a * b);
+  },
+  pow: function pow(a, b) {
+    console.log(a ** b);
+  },
 };
 
-player.sayHello("lynn");
-player.sayHello("sumin");
+calculator.add(1, 2);
+calculator.min(1, 2);
+calculator.div(1, 2);
+calculator.mul(1, 2);
+calculator.pow(1, 2);
