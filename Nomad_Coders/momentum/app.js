83ea8bdf766f2ee46a688b10458269d46fcb8302
sumin <tnals545@naver.com>
2022-06-19 18:28:18 +0900

    Events

--- Nomad_Coders/momentum/app.js
+++ Nomad_Coders/momentum/app.js
@@ -1,3 +1,39 @@
-const title = document.querySelectorAll(".hello h1");
+const h1 = document.querySelector(".hello:first-child h1");
 
-title.innerText = "Hello";
+function handleTitleClick() {
+  h1.style.color = "blue";
+  console.log("Title was clicked!");
+}
+
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI")
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOD")
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy)
+window.addEventListener("offline", handleWindowOffline)
+window.addEventListener("online", handleWindowOnline)
