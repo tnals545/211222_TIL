18be7af43cd318f027544a61fe2efbe00d9e418c
sumin <tnals545@naver.com>
2022-06-24 17:05:17 +0900

    Adding ToDos

--- Nomad_Coders/momentum/js/todo.js
+++ Nomad_Coders/momentum/js/todo.js
@@ -2,12 +2,19 @@ const toDoFrom = document.getElementById("todo-form");
 const toDoInput = toDoFrom.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
-  console.log(toDoInput.value);
   toDoInput.value = "";
-  console.log(newTodo, toDoInput.value);
+  paintToDo(newTodo);
 }
 
 toDoFrom.addEventListener("submit", handleToDoSubmit);
