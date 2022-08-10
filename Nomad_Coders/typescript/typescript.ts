50bf252a3de56c3718e7e08634d1cca592a8df96
sumin <tnals545@naver.com>
2022-08-10 19:08:23 +0900

    #2.2 Types of TS part One

--- Nomad_Coders/typescript/typescript.ts
+++ Nomad_Coders/typescript/typescript.ts
@@ -1,9 +1,16 @@
-let a = "hello";
-let b: boolean = false;
-let c: number[] = [];
-c.push(1);
-const player = {
-  name: "nico",
+// object의 타입을 명시적으로 정해줄 수 있다.
+// 하지만 아래의 Age와 Name처럼 너무 과하게 사용하는 것은 좋지 않다.
+// 코드가 깔끔해지고 효율적으로 사용할 수 있는 상황에서 사용하자.
+type Age = string;
+type Name = number;
+type Player = {
+  name: Age;
+  age?: Name;
 };
 
-player.name = 1;
+const playerMaker = (name: string): Player => ({ name });
+// name:string -> 입력되는 name의 타입 지정
+// : Player -> 출력되는 타입 지정
+
+const nico = playerMaker("nico");
+nico.age = 12;
