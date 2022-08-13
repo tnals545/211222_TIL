78561f50bf5b6ca3f35144b694d7d3ea0f91878a
sumin <tnals545@naver.com>
2022-08-14 03:30:35 +0900

    #4.1 Recap

--- Nomad_Coders/typescript/typescript.ts
+++ Nomad_Coders/typescript/typescript.ts
@@ -1,22 +1,35 @@
-abstract class User {
-  constructor(
-    protected firstName: string,
-    protected lastName: string,
-    protected nickName: string
-  ) {}
-  abstract getNickName(): void;
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
+type Words = {
+  [key: string]: string;
+};
+
+class Dict {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term: string) {
+    return this.words[term];
   }
 }
 
-class Player extends User {
-  getNickName() {
-    console.log(this.nickName);
-  }
+class Word {
+  constructor(public term: string, public def: string) {}
 }
 
-const nico = new Player("nico", "las", "니코");
+const kimchi = new Word("kimchi", "한국의 음식");
 
-nico.nickname;
-nico.getFullName();
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.def("kimchi");
+
+// challenge
+// 단어를 삭제하고, 단어를 업데이트하는 메소드를 만들고
+// Word 클래스에서는 단어의 정의를 추가하거나 수정하는 메소드,
+// 그리고 단어를 출력하는 메소드 같은걸 만들기
+// 그냥 클래스랑 메소드, 그리고 private나 public 같은 것들을 사용해서 뭔가를 만들어보기
