6ce44e995dea881e3d7bd3f4a8b2c5141f911c53
sumin <tnals545@naver.com>
2022-08-19 22:05:53 +0900

    #4.1 createAction

--- Front-End/redux/src/components/ToDo.js
+++ Front-End/redux/src/components/ToDo.js
@@ -10,9 +10,8 @@ const ToDo = ({ text, id }) => {
   };
   return (
     <li>
-      <Link to={`${id}`}>
-        {text} <button onClick={onBtnClick}>DEL</button>
-      </Link>
+      <Link to={`${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 };
