d5eef65b1b68146fd8134e21d17ae3927a98560e
sumin <tnals545@naver.com>
2022-08-19 19:43:47 +0900

    #3.5 Detail Screen

--- Front-End/redux/src/components/ToDo.js
+++ Front-End/redux/src/components/ToDo.js
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import React from "react";
 import { deleteToDo } from "../store";
+import { Link } from "react-router-dom";
 
 const ToDo = ({ text, id }) => {
   const dispatch = useDispatch();
@@ -9,7 +10,9 @@ const ToDo = ({ text, id }) => {
   };
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   );
 };
--- Front-End/redux/src/routes/Detail.js
+++ Front-End/redux/src/routes/Detail.js
@@ -1,1 +1,22 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+const Detail = () => {
+  const toDos = useSelector((state) => state);
+
+  //클릭된 리스트의 아이디값을 가져옴
+  const rim = useParams().id;
+
+  //아이디 같은것을 찾아냄
+  const toDoText = toDos.find((toDo) => toDo.id === parseInt(rim));
+  console.log(toDoText);
+
+  return (
+    <>
+      {toDoText.text}
+      Created at : {toDoText.id}
+    </>
+  );
+};
+
+export default Detail;
