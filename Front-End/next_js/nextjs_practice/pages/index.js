161d577350351634b4687ecf606d2546a736f5dc
sumin <tnals545@naver.com>
2022-08-20 12:42:36 +0900

    #2.0 Patterns ~ #2.1 Fetching Data

--- Front-End/next_js/nextjs_practice/pages/index.js
+++ Front-End/next_js/nextjs_practice/pages/index.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+import Seo from "../components/Seo";
+
+const API_KEY = "3abaebd400777ae5fa5711c8394abc37";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
-    <div className="active">
-      <h1>Hello</h1>
+    <div>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
