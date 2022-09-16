7eb8cd157525f00b5ed330e3dc328e8dd9a291f5
sumin <tnals545@naver.com>
2022-09-16 21:36:14 +0900

    #2.2 useClick

--- Front-End/react/10_practical_react_hooks/src/useEffect/index.js
+++ Front-End/react/10_practical_react_hooks/src/useEffect/index.js
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+// React 16.8v
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick !== "function") {
+      return;
+    }
+    // component 가 mount 될 때 eventListener 추가, dependency 가 없기 때문에 한 번만 실행됨
+    // => componentDidMount, componentDidUpdate
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    // component 가 mount 되지 않았을 때는 eventListener 가 배치되지 않게 함
+    // => componentWillUnMount
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []); // [] => dependency
+  return typeof onClick !== "function" ? undefined : element;
 };
 
 const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 5000);
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <div>Hi</div>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 };
