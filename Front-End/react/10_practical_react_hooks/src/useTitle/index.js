e3ad0e2446d177041d849c3e45cef7c14bb13e78
sumin <tnals545@naver.com>
2022-09-16 21:45:43 +0900

    useTitle

---
+++ Front-End/react/10_practical_react_hooks/src/useTitle/index.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+import { useTitle } from "./useTitle";
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div className="App">
+      <div>Hi</div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
