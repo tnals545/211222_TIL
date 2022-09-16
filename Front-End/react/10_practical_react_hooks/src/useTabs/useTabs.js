f40dc52efb7cecd6ed79a7fcfab0e3b1fe70a8a7
sumin <tnals545@naver.com>
2022-09-16 20:25:21 +0900

    #1.3 useTabs

--- Front-End/react/10_practical_react_hooks/src/useTabs/useTabs.js
+++ Front-End/react/10_practical_react_hooks/src/useTabs/useTabs.js
@@ -1,4 +1,6 @@
-const useTabs = (initialTab, allTabs) => {
+import { useState } from "react";
+
+export const useTabs = (initialTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initialTab);
   if (!allTabs || !Array.isArray(allTabs)) {
     return;
