09237fd2515e538ea7f66d3be53cdc6592296103
sumin <tnals545@naver.com>
2022-09-17 18:14:13 +0900

    #2.8 useAxios

---
+++ Front-End/react/10_practical_react_hooks/src/useAxios/index.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, error, data, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
