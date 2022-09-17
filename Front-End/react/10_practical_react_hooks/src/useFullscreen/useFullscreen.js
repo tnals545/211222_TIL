e8aaa2dc8771d0f3b004654aad1db3182e624091
sumin <tnals545@naver.com>
2022-09-17 16:57:46 +0900

    #2.6 useScroll & useFullscreen

--- Front-End/react/10_practical_react_hooks/src/useFullscreen/useFullscreen.js
+++ Front-End/react/10_practical_react_hooks/src/useFullscreen/useFullscreen.js
@@ -2,20 +2,27 @@ import { useRef } from "react";
 
 export const useFullscreen = (callback) => {
   const element = useRef();
+  // fullscreen 상태 console에 출력하는 function
   const runCb = (isFull) => {
     if (callback && typeof callback === "function") {
       callback(isFull);
     }
   };
+  // fullscreen으로 만들어주는 function
   const triggerFull = () => {
     if (element.current) {
       element.current.requestFullscreen();
       runCb(true);
     }
   };
+  // fullscreen 빠져나오는 function
   const exitFull = () => {
-    document.exitFullscreen();
-    runCb(false);
+    // fullscreen인지 check
+    const checkFullscreen = document.fullscreenElement;
+    if (checkFullscreen !== null) {
+      document.exitFullscreen();
+      runCb(false);
+    }
   };
   return { element, triggerFull, exitFull };
 };
