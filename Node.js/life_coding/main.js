0d26c2829fefd5a0f88c146bbd2f5ba7595c9d08
sumin <tnals545@naver.com>
2022-11-10 17:18:49 +0900

    Node.js-36.App 제작-글수정-파일명 변경, 내용저장

--- Node.js/life_coding/main.js
+++ Node.js/life_coding/main.js
@@ -139,6 +139,23 @@ var app = http.createServer(function (request, response) {
         }
       );
     });
+  } else if (pathName == "/update_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function (data) {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function (err) {
+        fs.writeFile(`data/${title}`, description, "utf-8", function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end("success");
+        });
+      });
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
