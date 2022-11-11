c7e17b2cc41e746b9913d74c09dd560d20e9d754
sumin <tnals545@naver.com>
2022-11-11 14:09:54 +0900

    Node.js43.App제작-템플릿 기능 정리정돈하기

--- Node.js/life_coding/main.js
+++ Node.js/life_coding/main.js
@@ -3,10 +3,11 @@ var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
 
-function templateHTML(title, list, body, control) {
-  return `
-  <!doctype html>
-    <html>
+var template = {
+  HTML: function (title, list, body, control) {
+    return `
+    <!doctype html>
+      <html>
         <head>
         <title>WEB1 - ${title}</title>
         <meta charset="utf-8">
@@ -17,20 +18,20 @@ function templateHTML(title, list, body, control) {
         ${control}
         ${body}
         </body>
-        </html>
-        `;
-}
-
-function templateList(filelist) {
-  var list = `<ul>`;
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
-  list = list + "</ul>";
-  return list;
-}
+      </html>
+          `;
+  },
+  list: function (filelist) {
+    var list = `<ul>`;
+    var i = 0;
+    while (i < filelist.length) {
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i = i + 1;
+    }
+    list = list + "</ul>";
+    return list;
+  },
+};
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -42,15 +43,15 @@ var app = http.createServer(function (request, response) {
       fs.readdir("./data", function (error, filelist) {
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var list = templateList(filelist);
-        var template = templateHTML(
+        var list = template.list(filelist);
+        var html = template.HTML(
           title,
           list,
           `<h2>${title}</h2>${description}`,
           `<a href="/create">create</a>`
         );
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       });
     } else {
       fs.readdir("./data", function (error, filelist) {
@@ -59,8 +60,8 @@ var app = http.createServer(function (request, response) {
           "utf-8",
           function (error, description) {
             var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(
+            var list = template.list(filelist);
+            var html = template.HTML(
               title,
               list,
               `<h2>${title}</h2>${description}`,
@@ -73,7 +74,7 @@ var app = http.createServer(function (request, response) {
               `
             );
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
           }
         );
       });
@@ -81,8 +82,8 @@ var app = http.createServer(function (request, response) {
   } else if (pathName == "/create") {
     fs.readdir("./data", function (error, filelist) {
       var title = "WEB - create";
-      var list = templateList(filelist);
-      var template = templateHTML(
+      var list = template.list(filelist);
+      var html = template.HTML(
         title,
         list,
         `
@@ -99,7 +100,7 @@ var app = http.createServer(function (request, response) {
         ""
       );
       response.writeHead(200);
-      response.end(template);
+      response.end(html);
     });
   } else if (pathName == "/create_process") {
     var body = "";
@@ -122,8 +123,8 @@ var app = http.createServer(function (request, response) {
         "utf-8",
         function (error, description) {
           var title = queryData.id;
-          var list = templateList(filelist);
-          var template = templateHTML(
+          var list = template.list(filelist);
+          var html = template.HTML(
             title,
             list,
             `
@@ -141,7 +142,7 @@ var app = http.createServer(function (request, response) {
             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
           );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         }
       );
     });
