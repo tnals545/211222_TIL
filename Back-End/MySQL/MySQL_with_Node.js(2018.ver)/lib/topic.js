47dcceaff54a696a517299bcd13e5fcf4e50dea1
sumin <tnals545@naver.com>
2022-11-12 18:46:20 +0900

    Node.js & MySQL - 14.3. 정리정돈 - topic 3

--- Back-End/MySQL/MySQL_with_Node.js(2018.ver)/lib/topic.js
+++ Back-End/MySQL/MySQL_with_Node.js(2018.ver)/lib/topic.js
@@ -109,3 +109,89 @@ exports.create_process = function (request, response) {
     );
   });
 };
+
+exports.update = function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  db.query(`SELECT * FROM topic`, function (err, topics) {
+    if (err) {
+      throw err;
+    }
+    db.query(
+      `SELECT * FROM topic WHERE id=?`,
+      [queryData.id],
+      function (err2, topic) {
+        if (err2) {
+          throw err2;
+        }
+        db.query(`SELECT * FROM author`, function (err2, authors) {
+          var list = template.list(topics);
+          var html = template.HTML(
+            topic[0].title,
+            list,
+            `
+                <form action="/update_process" method="post">
+                  <input type="hidden" name="id" value="${topic[0].id}">
+                  <p><input type="text" name="title" placeholder="title" value="${
+                    topic[0].title
+                  }"></p>
+                  <p>
+                    <textarea name="description" placeholder="description">${
+                      topic[0].description
+                    }</textarea>
+                  </p>
+                  <p>
+                    ${template.authorSelect(authors, topic[0].author_id)}
+                  </p>
+                  <p>
+                    <input type="submit">
+                  </p>
+                </form>
+                `,
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
+      }
+    );
+  });
+};
+
+exports.update_process = function (request, response) {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    db.query(
+      `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
+      [post.title, post.description, post.author, post.id],
+      function (err, result) {
+        if (err) {
+          throw err;
+        }
+        response.writeHead(302, { Location: `/?id=${post.id}` });
+        response.end();
+      }
+    );
+  });
+};
+
+exports.delete_process = function (request, response) {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    db.query(`DELETE FROM topic WHERE id=?`, [post.id], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      response.writeHead(302, { Location: `/` });
+      response.end();
+    });
+  });
+};
--- Back-End/MySQL/MySQL_with_Node.js(2018.ver)/main.js
+++ Back-End/MySQL/MySQL_with_Node.js(2018.ver)/main.js
@@ -20,87 +20,11 @@ var app = http.createServer(function (request, response) {
   } else if (pathname === "/create_process") {
     topic.create_process(request, response);
   } else if (pathname === "/update") {
-    db.query(`SELECT * FROM topic`, function (err, topics) {
-      if (err) {
-        throw err;
-      }
-      db.query(
-        `SELECT * FROM topic WHERE id=?`,
-        [queryData.id],
-        function (err2, topic) {
-          if (err2) {
-            throw err2;
-          }
-          db.query(`SELECT * FROM author`, function (err2, authors) {
-            var list = template.list(topics);
-            var html = template.HTML(
-              topic[0].title,
-              list,
-              `
-              <form action="/update_process" method="post">
-                <input type="hidden" name="id" value="${topic[0].id}">
-                <p><input type="text" name="title" placeholder="title" value="${
-                  topic[0].title
-                }"></p>
-                <p>
-                  <textarea name="description" placeholder="description">${
-                    topic[0].description
-                  }</textarea>
-                </p>
-                <p>
-                  ${template.authorSelect(authors, topic[0].author_id)}
-                </p>
-                <p>
-                  <input type="submit">
-                </p>
-              </form>
-              `,
-              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        }
-      );
-    });
+    topic.update(request, response);
   } else if (pathname === "/update_process") {
-    var body = "";
-    request.on("data", function (data) {
-      body = body + data;
-    });
-    request.on("end", function () {
-      var post = qs.parse(body);
-      db.query(
-        `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
-        [post.title, post.description, post.author, post.id],
-        function (err, result) {
-          if (err) {
-            throw err;
-          }
-          response.writeHead(302, { Location: `/?id=${post.id}` });
-          response.end();
-        }
-      );
-    });
+    topic.update_process(request, response);
   } else if (pathname === "/delete_process") {
-    var body = "";
-    request.on("data", function (data) {
-      body = body + data;
-    });
-    request.on("end", function () {
-      var post = qs.parse(body);
-      db.query(
-        `DELETE FROM topic WHERE id=?`,
-        [post.id],
-        function (err, result) {
-          if (err) {
-            throw err;
-          }
-          response.writeHead(302, { Location: `/` });
-          response.end();
-        }
-      );
-    });
+    topic.delete_process(request, response);
   } else {
     response.writeHead(404);
     response.end("Not found");
