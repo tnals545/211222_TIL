7b048e8fac2247b69f6c88993f19474845f3b81b
sumin <tnals545@naver.com>
2022-11-12 17:27:35 +0900

    Node.js & MySQL -11. MySQL JOIN - 글 수정 구현

--- Back-End/MySQL/MySQL_with_Node.js(2018.ver)/main.js
+++ Back-End/MySQL/MySQL_with_Node.js(2018.ver)/main.js
@@ -129,26 +129,35 @@ var app = http.createServer(function (request, response) {
           if (err2) {
             throw err2;
           }
-          var list = template.list(topics);
-          var html = template.HTML(
-            topic[0].title,
-            list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
+          db.query(`SELECT * FROM author`, function (err2, authors) {
+            var list = template.list(topics);
+            var html = template.HTML(
+              topic[0].title,
+              list,
+              `
+              <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${topic[0].id}">
+                <p><input type="text" name="title" placeholder="title" value="${
+                  topic[0].title
+                }"></p>
+                <p>
+                  <textarea name="description" placeholder="description">${
+                    topic[0].description
+                  }</textarea>
+                </p>
+                <p>
+                  ${template.authorSelect(authors, topic[0].author_id)}
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+              `,
+              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+            );
+            response.writeHead(200);
+            response.end(html);
+          });
         }
       );
     });
@@ -160,8 +169,8 @@ var app = http.createServer(function (request, response) {
     request.on("end", function () {
       var post = qs.parse(body);
       db.query(
-        `UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`,
-        [post.title, post.description, post.id],
+        `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
+        [post.title, post.description, post.author, post.id],
         function (err, result) {
           if (err) {
             throw err;
