4c794ff7342a57dc291c5ca1be2f0aaf65ccaef1
sumin <tnals545@naver.com>
2022-11-12 17:01:53 +0900

    Node.js & MySQL - 7.2. MySQL로 글수정 기능 구현 2

--- Back-End/MySQL/MySQL_with_Node.js(2018.ver)/main.js
+++ Back-End/MySQL/MySQL_with_Node.js(2018.ver)/main.js
@@ -150,15 +150,17 @@ var app = http.createServer(function (request, response) {
     });
     request.on("end", function () {
       var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-          response.writeHead(302, { Location: `/?id=${title}` });
+      db.query(
+        `UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`,
+        [post.title, post.description, post.id],
+        function (err, result) {
+          if (err) {
+            throw err;
+          }
+          response.writeHead(302, { Location: `/?id=${post.id}` });
           response.end();
-        });
-      });
+        }
+      );
     });
   } else if (pathname === "/delete_process") {
     var body = "";
